a8b0e8d7656609df37c0bbc6cc95f7936b33516b
an3-bit <andrewmogambi12@gmail.com>
2025-07-21 16:41:33 +0300

    index

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['https://stays-self.vercel.app', 'http://localhost:8080'] // Allow multiple origins
+  origin: ['https://stays-self.vercel.app', 'https://stays-self.vercel.app/', 'http://localhost:8080'] // Allow multiple origins
 }));
 
 const {
